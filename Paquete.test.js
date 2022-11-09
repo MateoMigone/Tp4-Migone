3bf9df7d4549e347f669b656b6f732842dd965b9
unknown <mateomigone01@gmail.com>
2022-11-09 17:26:46 -0300

    TDD iteration

--- Paquete.test.js
+++ Paquete.test.js
@@ -2,5 +2,5 @@ const Paquete = require('./Paquete');
 
 test("crear paquete", () => {
     paquete = new Paquete(1)
-    expect (paquete.destino()).toBe(1);
+    expect (paquete.destino).toBe(1);
 });
