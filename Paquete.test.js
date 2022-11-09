ad451ef9ed8d89bf144cefa617d8c91c7fc0ac82
unknown <mateomigone01@gmail.com>
2022-11-09 17:36:00 -0300

    TDD iteration

--- Paquete.test.js
+++ Paquete.test.js
@@ -4,3 +4,8 @@ test("crear paquete", () => {
     paquete = new Paquete(1)
     expect (paquete.destino).toBe(1);
 });
+
+test("crear paquete", () => {
+    paquete = new Paquete()
+    expect(paquete.destino).toThrow(new Error("Debe ingresar destino"));
+});
