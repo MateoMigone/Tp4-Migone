e9d594cdc351983ed51c5eaf0295d818f458e970
unknown <mateomigone01@gmail.com>
2022-11-09 18:39:44 -0300

    TDD iteration

--- Paquete.js
+++ Paquete.js
@@ -2,6 +2,9 @@ function Paquete (dest,lisProd){
     if (dest === undefined){
         throw new Error("Debe ingresar destino");
     }
+    if (lisProd === undefined){
+        throw new Error("Debe ingresar producto/s");
+    }
     this.destino = dest;
     this.listaProductos = lisProd;
 }
