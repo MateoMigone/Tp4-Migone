cb91facd5765333e0eeb3ac986024435ecd1f349
unknown <mateomigone01@gmail.com>
2022-11-10 23:29:42 -0300

    TDD iteration

--- CentroFacturacion.js
+++ CentroFacturacion.js
@@ -2,6 +2,7 @@ function CentroFacturacion(capCol){
     this.capacidadProcesamiento = 3;
     this.capacidadCola = capCol;
     this.colaEntrada = [];
+    this.colaSalida = [];
 }
 
 module.exports = CentroFacturacion;
