5006bc09597791b75b171c32c040373d15a207fd
unknown <mateomigone01@gmail.com>
2022-11-10 23:19:19 -0300

    TDD iteration

--- CentroFacturacion.test.js
+++ CentroFacturacion.test.js
@@ -1,6 +1,7 @@
 const CentroFacturacion = require('./CentroFacturacion');
 
 test("crear centro facturacion ", () => {
-    centroFacturacion = new CentroFacturacion();
+    centroFacturacion = new CentroFacturacion(5);
     expect(centroFacturacion.capacidadProcesamiento).toBe(3);
+    expect(centroFacturacion.capacidadCola).toBe(5);
 });
