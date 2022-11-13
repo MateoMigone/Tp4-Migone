8b096ba2b8cae12e9c088e475101744a36e20145
unknown <mateomigone01@gmail.com>
2022-11-13 19:48:55 -0300

    TDD iteration

--- Local.test.js
+++ Local.test.js
@@ -5,3 +5,13 @@ test("crear local", () => {
     expect(local.nombre).toBe("A");
     expect(local.colaSalida.length).toBe(0);
 });
+
+test("crear local", () => {
+    local = new Local("A");
+    local.generarPaquete(1,[["remera",4],["buzo",2]],6);
+    expect(local.colaSalida[0].listaProductos[0][0]).toBe("remera");
+    expect(local.colaSalida[0].listaProductos[0][1]).toBe(4);
+    expect(local.colaSalida[0].listaProductos[1][0]).toBe("buzo");
+    expect(local.colaSalida[0].listaProductos[1][1]).toBe(2);
+});
+
