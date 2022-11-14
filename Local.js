a96a0447876c8c0adbadd5b1a6b8b63b1d4690d9
unknown <mateomigone01@gmail.com>
2022-11-14 16:08:49 -0300

    TDD iteration

--- Local.js
+++ Local.js
@@ -4,7 +4,7 @@ function Local(nom){
     this.nombre = nom;
     this.colaSalida = [];
     this.generarPaquete = function (destino,listaProductos,urgencia){
-        if (isNan(Paquete.prototype.numero)){
+        if (isNaN(Paquete.prototype.numero)){
             Paquete.prototype.numero = 0;
         }
         paquete = new Paquete(destino,listaProductos,urgencia);
