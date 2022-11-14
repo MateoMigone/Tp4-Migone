c6f6bbbc44cf2fda7a1884bc24a363aeaae7488a
unknown <mateomigone01@gmail.com>
2022-11-14 15:40:14 -0300

    TDD iteration

--- Paquete.test.js
+++ Paquete.test.js
@@ -9,8 +9,19 @@ test("crear paquete", () => {
     expect(paquete.listaProductos[1][0]).toBe("buzo");
     expect(paquete.listaProductos[1][1]).toBe(2);
     expect(paquete.urgencia).toBe(6);
-    expect(paquete.numero).toBe(0)
 });
+
+test("crear dos paquetes con distintos numeros de paquete", () => {
+    Paquete.prototype.numero = 0;
+    paquete0 = new Paquete(1,[["remera",4],["buzo",2]],6);
+    paquete1 = new Paquete(2,[["libro",6],["lapicera",7]],6);
+    paquete2 = new Paquete(3,[["bolso",1],["collar",2]],4);
+    expect(paquete0.numero).toBe(0);
+    expect(paquete1.numero).toBe(1);
+    expect(paquete2.numero).toBe(2);
+});
+
+
 
 test("error al crear paquete sin destino, productos ni urgencia", () => {
     expect(() => {
