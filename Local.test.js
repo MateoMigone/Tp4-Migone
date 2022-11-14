8d78d3a8b5ed5d3efbf2529ea606deb08cfeb8e4
unknown <mateomigone01@gmail.com>
2022-11-14 18:59:53 -0300

    TDD iteration

--- Local.test.js
+++ Local.test.js
@@ -31,6 +31,7 @@ test("generar paquetes en distintos locales", () => {
 });
 
 test("pasar paquete", () => {
+    localA.generarPaquete(1,[["remera",4],["buzo",2]],8);
     localA.pasarPaquete(centroFacturacion);
     expect(localA.colaSalida.length).toBe(0);
     expect(centroFacturacion.colaEntrada.length).toBe(1);
