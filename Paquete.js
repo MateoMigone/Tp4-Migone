30eb90d41af1a0a964d53984f3382030bd3121fe
unknown <mateomigone01@gmail.com>
2022-11-21 22:37:50 -0300

    TDD iteration

--- Paquete.js
+++ Paquete.js
@@ -1,15 +1,8 @@
 function Paquete (dest,urg){
-    if (dest === undefined || lisProd === undefined || urg === undefined){
-        throw new Error("Debe ingresar todos los datos necesarios");
-    }
-    if (lisProd.length == 0){
-        throw new Error("Debe ingresar lista con un producto por lo menos");
-    }
     if (isNaN(Paquete.prototype.numero)){
         Paquete.prototype.numero = 0;
     }
     this.destino = dest;
-    this.listaProductos = lisProd;
     this.urgencia = urg;
     this.numero = Paquete.prototype.numero++;
 }
