65b56e9465a5fac3eec6c32af4a26d0f6df406e3
unknown <mateomigone01@gmail.com>
2022-11-21 22:36:40 -0300

    TDD iteration

--- PaqueteSimple.js
+++ PaqueteSimple.js
@@ -2,13 +2,14 @@ const Paquete = require('./Paquete');
 
 function PaqueteSimple (dest,lisProd,urg){
     Paquete.call(this,dest,urg);
+    this.listaProductos = lisProd;
     if (dest === undefined || lisProd === undefined || urg === undefined){
         throw new Error("Debe ingresar todos los datos necesarios");
     }
     if (lisProd.length == 0){
         throw new Error("Debe ingresar lista con un producto por lo menos");
     }
-    this.listaProductos = lisProd;
+    
 }
 
 PaqueteSimple.prototype = Object.create(Paquete.prototype);
