4cd37b7880d6ef77c5837c5095013898d78f5d49
unknown <mateomigone01@gmail.com>
2022-11-21 23:12:46 -0300

    TDD iteration

--- PaqueteUnificado.js
+++ PaqueteUnificado.js
@@ -4,6 +4,14 @@ function PaqueteUnificado (paquetes){
     Paquete.call(this);
     this.listaPaquetes = [];
     this.destino = paquetes[0].destino;
+    this.listaPaquetes.push(paquetes[0]); 
+    var urg = paquetes[0].urgencia;
+    for(i = 1; i < paquetes.length; i++){
+        this.listaPaquetes.push(paquetes[i]);
+        if (paquetes[i].urgencia < urg){
+            urg = paquetes[i].urgencia
+        }
+    }
 }
 
 PaqueteUnificado.prototype = Object.create(Paquete.prototype);
