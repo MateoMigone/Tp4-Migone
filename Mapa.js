708d16b0e9c5b993129ed6d4f39ae3f25f49e66f
unknown <mateomigone01@gmail.com>
2022-11-23 21:10:26 -0300

    TDD iteration

--- Mapa.js
+++ Mapa.js
@@ -4,10 +4,11 @@ function Mapa(mat){
     this.matriz = mat;
 
     this.avanzarTiempo = function(reloj){
-        reloj.avanzarTiempo();
-        for (let i=this.matriz[0].length; i>0; i--){
-            this.matriz[0][i].avanzarTiempo(this.matriz[0][i+1],reloj);
-        }
+        // reloj.avanzarTiempo();
+        // for (let i=this.matriz[0].length; i>0; i--){
+        //     this.matriz[0][i].avanzarTiempo(this.matriz[0][i+1],reloj);
+        // }
+        return this.matriz[0][0];
     }
 }
 
